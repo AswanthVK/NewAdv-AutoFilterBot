15a15d669092f1994453c04b6aea8f57e36f5c01
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2021-08-06 08:47:37 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -9,8 +9,7 @@ class Translation(object):
 
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+For More Details Click <i>Help</i> Button Below..
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +42,15 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/AswanthVK">AswanthVK</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/AswanthVK/Adv-Auto-Filter-Bot">Click Me</a></i>
 """
