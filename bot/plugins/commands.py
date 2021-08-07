e1c5747ec9f8936497512f5b69f16c2d0a9d3125
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2021-08-07 14:38:40 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -56,7 +56,7 @@ async def start(bot, update):
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await bot.send_message(LOG_CHANNEL, text=f"#NEW_USER: \n\nUser {update.from_user.first_name} started Bot!!")
+    #await bot.send_message(chat_id=LOG_CHANNEL, text=f"#NEW_USER: \n\nUser {update.from_user.first_name} started Bot!!")
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT.format(
