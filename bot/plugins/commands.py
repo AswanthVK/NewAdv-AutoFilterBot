d0091a977d51956e5f9fd7146c9a61d316ef3f48
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2021-08-07 13:01:11 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -57,7 +57,7 @@ async def start(bot, update):
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await bot.send_message(
-        LOG_CHANNEL,
+        chat_id=LOG_CHANNEL,
         f"**New User Joined:** \n\nUser [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started Bot!!"
     )
     await bot.send_message(
